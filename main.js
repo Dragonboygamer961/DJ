bb936f352556693ce8f02ac1e0853aa44d0e4489
Dragonboygamer961 <95893470+Dragonboygamer961@users.noreply.github.com>
2022-10-24 16:52:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+my_music = "";
+
+function perload(){
+    my_music = loadSound("music.mp3");
+}
+
+function setup(){
+
+    canvas = createCanvas(250, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw() {
+ image(video, 0,0, 250,250);
+}
+
+function PLAY_MUSIC() {
+    
+    my_music.play();
+}
